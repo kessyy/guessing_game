2e00215303b9e364bf4d9e5c13919baf093fbf24
kessyy <kather946@gmail.com>
2019-01-26 15:51:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-number = "4"
-guess = ""
-count = 0
+guess_number = int("4")
+guesses = ""
+guess_count = 0
 max_guesses = False
+name = str(input("Enter your name: "))
 
-while guess != number and not max_guesses:
-    if count < 3:
-        guess = input("enter number: ")
-        count += 1
+print("Well Hello " + name + " I am thinking of a number: can you guess it?")
 
+while guesses != guess_number and not max_guesses:
+    if guess_count < 3:
+        try:
+            guesses = int(input("enter number: "))
+        except ValueError:
+            print("please input number value")
+        guess_count += 1
     else:
         max_guesses = True
-
 if max_guesses:
-    print("Sorry, try again")
+    print("Oops, try again")
 
 else:
     print("you win")
